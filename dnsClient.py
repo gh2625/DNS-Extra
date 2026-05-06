fd658cb0b875cc20cdfe2d5bfe832d94eab35ba4
unknown <gh2525@nyu.edu>
2026-05-05 20:44:12 -0500

    remove prints

--- dnsClient.py
+++ dnsClient.py
@@ -153,12 +153,10 @@ def dns_query(type, name, server):
         if type == 1: # Lookup Type value
             # A record (IPv4 address)
             ipv4 = socket.inet_ntop(socket.AF_INET, rdata)
-            print(f'{name} has IPv4 address {ipv4}')\
             return ipv4
         elif type == 28: # Lookup Type value
             # AAAA record (IPv6 address)
             ipv6 = socket.inet_ntop(socket.AF_INET6, rdata)
-            print(f'{name} has IPv6 address {ipv6}')
             return ipv6                
 
 def parse_name(data, offset):
